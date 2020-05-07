0be2ae46f48da1028110304f592fe20f928ac587
linetor <linetor@gmail.com>
2020-05-08 01:09:07 +0900

    [TEST-003] 1 day 1 commit. Is it worth to do like this?

--- recoding_by_shell.py
+++ recoding_by_shell.py
@@ -1,6 +1,96 @@
 import subprocess
 import datetime
 import argparse
+import dropbox
+import sys
+from configparser import ConfigParser
+import os
+import time
+
+#to do : need to refactor code style
+parser = ConfigParser()
+parser.read('.config')
+api_token=parser.get('dropbox', 'api_token')
+upload_loc=parser.get('dropbox', 'upload_loc')
+move_loc = parser.get('dropbox', 'move_loc')
+
+dbx = dropbox.Dropbox(api_token)
+
+
+
+def move_and_wait_until_complete(reloc_paths):
+    if len(reloc_paths) == 0:
+        print("Nothing to move")
+        return
+    print("{} files to move...".format(len(reloc_paths)))
+    job_status = dbx.files_move_batch(reloc_paths, autorename=True)
+    if not job_status.is_async_job_id():
+        print("Job already complete!")
+        return
+    jobid = job_status.get_async_job_id()
+    print("Executing with jobid({})".format(jobid))
+    print("Checking status: ")
+    while 1:
+        status = dbx.files_move_batch_check(jobid)
+        if not (status.is_complete() or status.is_failed()):
+            sys.stdout.write("...")
+            sys.stdout.flush()
+            time.sleep(1)
+            continue
+
+        if status.is_complete():
+            print("\nJob id ({}) complete".format(jobid))
+            print("{} files moved".format(len(reloc_paths)))
+        else:
+            print("\nJob id ({}) failed".format(jobid))
+            print(status.get_failed())
+        break
+
+
+def get_file_names_to_move(from_folder):
+    continuing_list = dbx.files_list_folder( from_folder)
+    files = [_file.path_lower for _file in continuing_list.entries]
+    if continuing_list.has_more:
+        files.extend(get_file_names_to_move(continuing_list.cursor))
+    return files
+
+    """
+    if cursor is not None:
+        continuing_list = dbx.files_list_folder_continue(cursor)
+    else:
+        continuing_list = dbx.files_list_folder( upload_loc)
+
+    files = [_file.path_lower for _file in continuing_list.entries]
+    if continuing_list.has_more:
+        files.extend(get_file_names_to_move(continuing_list.cursor))
+    return files
+    """
+
+
+def upload_to_dropbox():
+    import dropbox
+    from configparser import ConfigParser
+    parser = ConfigParser()
+    parser.read('.config')
+    api_token=parser.get('dropbox', 'api_token')
+    upload_loc=parser.get('dropbox', 'upload_loc')
+    move_loc=parser.get('dropbox', 'move_loc')
+    dbx = dropbox.Dropbox(api_token)
+    print(dbx.users_get_current_account())
+    with open("requirements.txt",'rb') as f:
+        dbx.files_upload(f.read(), upload_loc+"/requirements.txt", mode=dropbox.files.WriteMode.overwrite)
+
+    fileList=get_file_names_to_move(upload_loc)
+    print(fileList)
+    #no duplicate folder name
+    for x in fileList:
+        print(x,x.replace(upload_loc,move_loc))
+    relocation_paths=[dropbox.files.RelocationPath(x,x.replace(upload_loc,move_loc)) for x in fileList]
+    print(relocation_paths)
+    move_and_wait_until_complete(relocation_paths)
+    #list(map(lambda x: dropbox.files.RelocationPath(x,os.path.join('/ebs_past', "requirements.txt")), ["/ebs_today/requirements.txt"]))
+
+
 
 
 def recording():
@@ -15,6 +105,8 @@ def recording():
 
     parser.add_argument('duration', type=int, default=20,
                         help="What is the second number?")
+
+
 
     args = parser.parse_args()
 
@@ -39,4 +131,4 @@ def recording():
 
 
 if __name__ == "__main__":
-    recording()
+    upload_to_dropbox()
