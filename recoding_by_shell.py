43b7c4b6c7fca571a76f953928931c9110e56815
linetor <linetor@gmail.com>
2021-11-13 23:21:31 +0900

    [TEST-001] airflow recording test2

--- recoding_by_shell.py
+++ recoding_by_shell.py
@@ -6,12 +6,12 @@ import sys
 from configparser import ConfigParser
 import time
 
+
 # Is it need to be here ??
 # current_loc = configparser.get('current_loc', 'current_loc')
 
 
-def move_and_wait_until_complete(reloc_paths):
-
+def move_and_wait_until_complete(reloc_paths, dbx_variable):
     if len(reloc_paths) == 0:
         print("Nothing to move")
         return
@@ -19,7 +19,7 @@ def move_and_wait_until_complete(reloc_paths):
     print("{} files to move...".format(len(reloc_paths)))
     # files_move_batch is deprecated and need to change to move_batch.
     # but there is no move_batch api?
-    job_status = dbx.files_move_batch(reloc_paths, autorename=True)
+    job_status = dbx_variable.files_move_batch(reloc_paths, autorename=True)
     if not job_status.is_async_job_id():
         print("Job already complete!")
         return
@@ -27,7 +27,7 @@ def move_and_wait_until_complete(reloc_paths):
     print("Executing with jobid({})".format(jobid))
     print("Checking status: ")
     while 1:
-        status = dbx.files_move_batch_check(jobid)
+        status = dbx_variable.files_move_batch_check(jobid)
         if not (status.is_complete() or status.is_failed()):
             sys.stdout.write("...")
             sys.stdout.flush()
@@ -43,7 +43,7 @@ def move_and_wait_until_complete(reloc_paths):
         break
 
 
-def get_file_names_to_move(from_folder,date_str):
+def get_file_names_to_move(from_folder, date_str_variable, dbx_variable):
     # original code
     """
     if cursor is not None:
@@ -56,62 +56,59 @@ def get_file_names_to_move(from_folder,date_str):
         files.extend(get_file_names_to_move(continuing_list.cursor))
     return files
     """
-    continuing_list = dbx.files_list_folder(from_folder)
-    files = [_file.path_lower for _file in continuing_list.entries if date_str[:10] not in _file.name]
+    continuing_list = dbx_variable.files_list_folder(from_folder)
+    files = [_file.path_lower for _file in continuing_list.entries if date_str_variable[:10] not in _file.name]
     if continuing_list.has_more:
-        files.extend(get_file_names_to_move(continuing_list.cursor,date_str))
+        files.extend(get_file_names_to_move(continuing_list.cursor, date_str_variable, dbx_variable))
     return files
 
-def get_file_names_to_2weekago(from_folder):
-    continuing_list = dbx.files_list_folder(from_folder)
+
+def get_file_names_to_2weekago(from_folder, dbx_variable, date_str_variable):
+    continuing_list = dbx_variable.files_list_folder(from_folder)
     days_14_ago = datetime.datetime.now() - datetime.timedelta(weeks=2)
     days_14_ago_str = days_14_ago.strftime("%Y-%m-%d")
     files = [_file.path_lower for _file in continuing_list.entries if days_14_ago_str > _file.name]
     if continuing_list.has_more:
-        files.extend(get_file_names_to_move(continuing_list.cursor,date_str))
+        files.extend(get_file_names_to_move(continuing_list.cursor, date_str_variable, dbx_variable))
     return files
 
 
-def upload_to_dropbox(dbx,upload_loc,date_str,move_loc,program_name):
-
+def upload_to_dropbox(dbx_variable, upload_loc_var, date_str_variable, move_loc_var, program_name_var):
     # checking status
-    print(dbx.users_get_current_account())
+    print(dbx_variable.users_get_current_account())
 
-    filelist = get_file_names_to_move(upload_loc,date_str)
+    filelist = get_file_names_to_move(upload_loc_var, date_str_variable, dbx_variable)
     print(filelist)
     # need to no duplicate folder name
     for x in filelist:
-        print(x, x.replace(upload_loc, move_loc))
-    relocation_paths = [dropbox.files.RelocationPath(x,x.replace(upload_loc,move_loc)) for x in filelist]
+        print(x, x.replace(upload_loc_var, move_loc_var))
+    relocation_paths = [dropbox.files.RelocationPath(x, x.replace(upload_loc_var, move_loc_var)) for x in filelist]
     print(relocation_paths)
-    move_and_wait_until_complete(relocation_paths)
+    move_and_wait_until_complete(relocation_paths, dbx_variable)
 
-    m4a_file = date_str + '_' + program_name + '.m4a'
+    m4a_file = date_str_variable + '_' + program_name_var + '.m4a'
 
-    with open(current_loc+m4a_file, 'rb') as f:
-        dbx.files_upload(f.read(), upload_loc+'/' + m4a_file, mode=dropbox.files.WriteMode.overwrite)
+    with open(current_loc + m4a_file, 'rb') as f:
+        dbx_variable.files_upload(f.read(), upload_loc_var + '/' + m4a_file, mode=dropbox.files.WriteMode.overwrite)
 
     import os
-    os.system("mkdir "+current_loc+"past/"+date_str)
-    os.system("mv "+current_loc+m4a_file + " " +current_loc+"past/"+date_str+"/"+m4a_file)
+    os.system("mkdir " + current_loc + "past/" + date_str_variable)
+    os.system("mv " + current_loc + m4a_file + " " + current_loc + "past/" + date_str_variable + "/" + m4a_file)
 
-
-    delete_filelist = get_file_names_to_2weekago(move_loc)
+    delete_filelist = get_file_names_to_2weekago(move_loc_var, dbx_variable, date_str_variable)
 
     print(delete_filelist)
     for path in delete_filelist:
-        dbx.files_delete(path)
-
+        dbx_variable.files_delete(path)
 
 
 # checking : In main, variable is used for global variable
 # def recording(var_date_str, var_program_name, var_record_mins):
-def recording():
-
+def recording(date_str_variable):
     radio_addr = radio_address
 
-    ori_file = current_loc + date_str + '_' + program_name
-    m4a_file = current_loc + date_str + '_' + program_name + '.m4a'
+    ori_file = current_loc + date_str_variable + '_' + program_name
+    m4a_file = current_loc + date_str_variable + '_' + program_name + '.m4a'
 
     rtmpdump = ['rtmpdump', '-r', radio_addr, '-B', record_mins, '-o', ori_file]
     ffmpeg = ['ffmpeg', '-i', ori_file, '-vn', '-acodec', 'copy', m4a_file]
@@ -126,14 +123,13 @@ def recording():
 
 
 if __name__ == "__main__":
-
     date = datetime.datetime.now()
 
     date_str = date.strftime('%Y-%m-%d_%H:%M')
 
     argparser = argparse.ArgumentParser()
 
-    argparser.add_argument('program_name', type=str, default=date_str+"_english_radio",
+    argparser.add_argument('program_name', type=str, default=date_str + "_english_radio",
                            help="What is the ebs radio program name?")
 
     argparser.add_argument('radio_channel', type=str, default="ebs_fm",
@@ -145,7 +141,6 @@ if __name__ == "__main__":
     argparser.add_argument('current_loc', type=str, default="~/",
                            help="What is the current folder")
 
-
     args = argparser.parse_args()
 
     current_loc = args.current_loc
@@ -153,7 +148,7 @@ if __name__ == "__main__":
     record_mins = str(args.duration)
 
     configparser = ConfigParser()
-    configparser.read(current_loc+'.config')
+    configparser.read(current_loc + '.config')
     api_token = configparser.get('dropbox', 'api_token')
     upload_loc = configparser.get('dropbox', 'upload_loc')
     move_loc = configparser.get('dropbox', 'move_loc')
@@ -161,5 +156,5 @@ if __name__ == "__main__":
     radio_address = configparser.get('ebs_address', args.radio_channel)
 
     dbx = dropbox.Dropbox(api_token)
-    recording()
-    upload_to_dropbox(dbx,upload_loc,date_str,move_loc,program_name)
+    recording(date_str)
+    upload_to_dropbox(dbx, upload_loc, date_str, move_loc, program_name)
